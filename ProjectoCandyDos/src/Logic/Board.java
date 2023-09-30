78a9e1b2f042098bb52e42c29c20ba1d185407a8
Franco Piumatti <francopiumatti2004@gmail.com>
2023-09-30 16:44:51 -0300

    Metodos publicos board

--- ProjectoCandyDos/src/Logic/Board.java
+++ ProjectoCandyDos/src/Logic/Board.java
@@ -6,6 +6,49 @@ public class Board {
 	private int posX, posY, rows, columns;
 	private Block[][] matriz;
 	private Game myGame;
+	
+	
+	public int getRows() {
+		return rows;
+	}
+	
+	public int getColumns() {
+		return columns;
+	}
+	
+	public void fillBoard() {
+		
+	}
+	public void movePlayerDirection(int direction) {
+		switch(direction) {
+		case Juego.ABAJO:{
+			mover_jugador_auxiliar(posY + 1, posX);
+			break;
+		}
+		case Juego.ARRIBA:{
+			mover_jugador_auxiliar(posY - 1, posX);
+			break;
+		}
+		case Juego.IZQUIERDA:{
+			mover_jugador_auxiliar(posY, posX - 1);
+			break;
+		}
+		case Juego.DERECHA:{
+			mover_jugador_auxiliar(posY, posX + 1);
+			break;
+		}
+		}
+	}
+	public PositionList<Equivalent> swap(int direction){
+		
+	}
+	public Block getBlock(int row, int column) {
+		
+	}
+	public PositionList<Equivalent> destroyEntities(PositionList<Block> l){
+		
+	}
+	
 	private void setPlayerPosition(int newRow, int newColumn)
 	{
 		posY = newRow;
