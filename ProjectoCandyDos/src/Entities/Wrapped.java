b17fc6e5efed36fb8fd2fa91e15ff6c6fe1d186d
nicosagasti <nicolasferminsagasti@gmail.com>
2023-09-30 20:11:22 -0300

    cambia

--- ProjectoCandyDos/src/Entities/Wrapped.java
+++ ProjectoCandyDos/src/Entities/Wrapped.java
@@ -7,83 +7,74 @@ import Logic.Board;
 
 public class Wrapped extends Entity {
 
-    /* Constructor */
-    public Wrapped(int posRow, int posColumn, Colour colour) {
-        this.posRow = posRow;
-        this.posColumn = posColumn;
-        this.colour = colour;
-    }
+	/* Constructor */
+	public Wrapped(int posRow, int posColumn, Colour colour) {
+		this.posRow = posRow;
+		this.posColumn = posColumn;
+		this.colour = colour;
+	}
 
-    /* Methods */
-    @Override
-    public boolean isEquivalent(Entity e) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	/* Methods */
+	@Override
+	public boolean isEquivalent(Entity e) {
+		return e.equals(this);
+	}
 
-    @Override
-    public boolean equals(Candy c) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean equals(Candy c) {
+		return false;
+	}
 
-    @Override
-    public boolean equals(Glazed g) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean equals(Glazed g) {
+		return false;
+	}
 
-    @Override
-    public boolean equals(Wrapped w) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean equals(Wrapped w) {
+		return w.getColour() == colour;
+	}
 
-    @Override
-    public boolean equals(Stripped s) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean equals(Stripped s) {
+		return false;
+	}
 
-    @Override
-    public boolean equals(Jelly j) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean equals(Jelly j) {
+		return false;
+	}
 
-    @Override
-    public boolean equals(Empty e) {
-        return false;
-    }
+	@Override
+	public boolean equals(Empty e) {
+		return false;
+	}
 
-    @Override
-    public String getImage() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public String getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public boolean isSwappable(Entity e) {
-        return e.canReceive(this);
-    }
+	@Override
+	public boolean isSwappable(Entity e) {
+		return e.canReceive(this);
+	}
 
-    @Override
-    public boolean canReceive(Candy c) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean canReceive(Candy c) {
+		return true;
+	}
 
-    @Override
-    public boolean canReceive(Glazed g) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean canReceive(Glazed g) {
+		return false;
+	}
 
-    @Override
-    public boolean canReceive(Stripped s) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean canReceive(Stripped s) {
+		return true;
+	}
 
 	@Override
 	public boolean canReceive(Wrapped w) {
@@ -95,4 +86,4 @@ public class Wrapped extends Entity {
 	public List<Equivalent> getDestroyables(Board b) {
 		return null;
 	}
-
+}
