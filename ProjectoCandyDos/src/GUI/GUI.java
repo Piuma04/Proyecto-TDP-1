981ed0247a1f2661e4af8d32f2f8d65aace472cd
Basher259 <baltasarschwerdt@gmail.com>
2023-09-30 18:07:06 -0300

    GUI TERMINADA

--- ProjectoCandyDos/src/GUI/GUI.java
+++ ProjectoCandyDos/src/GUI/GUI.java
@@ -1,41 +1,84 @@
 package GUI;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+public class GUI extends JFrame implements KeyListener {
+    private final int numRows = 8;
+    private final int numCols = 8;
+    private final JButton[][] candyButtons = new JButton[numRows][numCols];
 
-import java.awt.EventQueue;
+    public GUI() {
+        setTitle("Candy Crush");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(numRows, numCols));
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+        // Agregar el KeyListener para la navegación
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
 
-public class GUI extends JFrame {
+        // Crear botones con caramelos aleatorios
+        Random random = new Random();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                JButton button = new JButton(getRandomCandy());
+                candyButtons[i][j] = button;
+                add(button);
+            }
+        }
 
-	private JPanel contentPane;
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					GUI frame = new GUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    // Generar un caramelo aleatorio
+    private String getRandomCandy() {
+        String[] candyTypes = {"Caramelo1", "Caramelo2", "Caramelo3", "Caramelo4", "Caramelo5"};
+        Random random = new Random();
+        int index = random.nextInt(candyTypes.length);
+        return candyTypes[index];
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public GUI() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    @Override
+    public void keyTyped(KeyEvent e) {}
 
-		setContentPane(contentPane);
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        int currentRow = 0;
+        int currentCol = 0;
 
+        // Encuentra la celda actualmente enfocada
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (candyButtons[i][j].isFocusOwner()) {
+                    currentRow = i;
+                    currentCol = j;
+                }
+            }
+        }
+
+        // Moverse a través de las celdas con las teclas de flecha
+        if (keyCode == KeyEvent.VK_LEFT && currentCol > 0) {
+            candyButtons[currentRow][currentCol - 1].requestFocus();
+        } else if (keyCode == KeyEvent.VK_RIGHT && currentCol < numCols - 1) {
+            candyButtons[currentRow][currentCol + 1].requestFocus();
+        } else if (keyCode == KeyEvent.VK_UP && currentRow > 0) {
+            candyButtons[currentRow - 1][currentCol].requestFocus();
+        } else if (keyCode == KeyEvent.VK_DOWN && currentRow < numRows - 1) {
+            candyButtons[currentRow + 1][currentCol].requestFocus();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GUI());
+    }
 }
+
+
