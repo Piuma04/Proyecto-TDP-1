2ec0be9bee9fd382683a102547f3a5abd9ccac6b
Franco Piumatti <francopiumatti2004@gmail.com>
2023-09-30 16:35:20 -0300

    Metodos lista

--- ProjectoCandyDos/src/Logic/Board.java
+++ ProjectoCandyDos/src/Logic/Board.java
@@ -1,5 +1,32 @@
 package Logic;
 
 public class Board {
-
+	private int posX, posY, rows, columns;
+	private Block matriz [][];
+	private Game myGame;
+	
+	
+	public int getRows() {
+		return rows;
+	}
+	
+	public int getColumns() {
+		return rows;
+	}
+	
+	public void fillBoard() {
+		
+	}
+	public void movePlayerDirection(int direction) {
+		
+	}
+	public PositionList<Equivalent> swap(int direction){
+		
+	}
+	public Block getBlock(int row, int column) {
+		
+	}
+	public PositionList<Equivalent> destroyEntities(PositionList<Block> l){
+		
+	}
 }
