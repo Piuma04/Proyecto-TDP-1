864c5f738900627dcdc8a2ca649b6ed4801a59e6
Santino Di Federico <Santinodifederico@gmail.com>
2023-09-30 23:01:14 -0300

    console view format modified

--- ProjectoCandyDos/src/Entities/Candy.java
+++ ProjectoCandyDos/src/Entities/Candy.java
@@ -87,5 +87,5 @@ public class Candy extends Entity {
 		return null;
 	}
 
-    public String toString() { return setStringColor("Candy("+this.posRow+", "+this.posColumn+")"); }
+    public String toString() { return setStringColor("C"); }
 }
