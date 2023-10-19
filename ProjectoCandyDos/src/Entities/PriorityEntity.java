e703217e717d2ee5c4bd7b5ccb89f2df006124ef
Santino Di Federico <Santinodifederico@gmail.com>
2023-10-18 21:09:54 -0300

    setup real lvl 1

--- ProjectoCandyDos/src/Entities/PriorityEntity.java
+++ ProjectoCandyDos/src/Entities/PriorityEntity.java
@@ -1,10 +1,10 @@
 package Entities;
 
 public class PriorityEntity {
-    
+
     private Entity entity;
     private int priority;
-    
+
     public PriorityEntity(Entity entity, int priority) {
         this.entity = entity;
         this.priority = priority;
