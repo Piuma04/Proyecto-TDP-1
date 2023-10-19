0ac09b590cd6b2c9957e5ca41f200b447760cff6
Piuma04 <132784525+Piuma04@users.noreply.github.com>
2023-10-19 12:40:11 -0300

    Update Goal.java

--- ProjectoCandyDos/src/Logic/Goal.java
+++ ProjectoCandyDos/src/Logic/Goal.java
@@ -1,6 +1,6 @@
 package Logic;
 
-import java.util.List;
+
 
 import Interfaces.Equivalent;
 
@@ -8,23 +8,30 @@ public class Goal {
     private int counter;
     private Equivalent type;
 
+   
     public Goal(int amount, Equivalent toDestroyEntityType) {
         counter = amount;
         type = toDestroyEntityType;
+    
     }
+   
 
     /**
      * 
      * @param equivalentList list of {@link Equivalent} to compare ALL of them to {@code this.type}
      * @return {@code true} if goal is reached.
      */
-    public boolean updateCounter(List<Equivalent> equivalentList) {
-        for (Equivalent entity : equivalentList) {
-            if (entity.isEquivalent(type))
-                counter--;
-            
-        }
-        return counter <= 0;
+    public void updateCounter(Equivalent equivalent) {
+        //System.out.println(equivalentList.toString());
+    	if(counter >0&& type.isEquivalent(equivalent)) {
+    		counter--;
+    	}
+    
+        
+    }
+    
+    public boolean finished() {
+    	return counter <= 0;
     }
     public int amountMissing() { return counter; }
     public String typeOfEntity() { return type.getImage(); }
