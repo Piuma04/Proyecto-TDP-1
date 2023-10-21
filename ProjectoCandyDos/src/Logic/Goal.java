9a583c4cd07b32f3651122712ff08ee8c74d42d8
Santino Di Federico <Santinodifederico@gmail.com>
2023-10-20 23:05:49 -0300

    Remove simple extra logs

--- ProjectoCandyDos/src/Logic/Goal.java
+++ ProjectoCandyDos/src/Logic/Goal.java
@@ -18,12 +18,6 @@ public class Goal {
      * @return {@code true} if goal is reached.
      */
     public void updateCounter(Equivalent equivalent) {
-        // System.out.println(equivalentList.toString());
-        
-        System.out.print("MYTYPE: " + type + " ");
-        System.out.print("Candy: ");
-        System.out.println(equivalent);
-        System.out.println(type.isEquivalent(equivalent));
         if (counter > 0 && type.isEquivalent(equivalent))
             counter--;
     }
