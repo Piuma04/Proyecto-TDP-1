edbc273b455386ea4677d9485d14730e638f414d
Basher259 <baltasarschwerdt@gmail.com>
2023-10-20 22:19:42 -0300

    cambiado nombre de el has Booster

---
+++ ProjectoCandyDos/src/Interfaces/SpecialDestroy.java
@@ -0,0 +1,17 @@
+package Interfaces;
+
+import Entities.Candy;
+import Entities.Empty;
+import Entities.Entity;
+import Entities.Glazed;
+import Entities.Stripped;
+import Entities.Wrapped;
+
+public interface SpecialDestroy {
+    public boolean isSpecialSwap(Entity e);
+    public boolean hasSpecialExplosion(Candy c);
+    public boolean hasSpecialExplosion(Stripped s);
+    public boolean hasSpecialExplosion(Wrapped w);
+    public boolean hasSpecialExplosion(Glazed g);
+    public boolean hasSpecialExplosion(Empty e);
+}
