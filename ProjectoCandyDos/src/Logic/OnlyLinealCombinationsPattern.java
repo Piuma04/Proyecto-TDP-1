6e4a815905df915bed026b1e53b8125354fa640d
Franco Piumatti <francopiumatti2004@gmail.com>
2023-11-01 20:43:47 -0300

    small change

--- ProjectoCandyDos/src/Logic/OnlyLinealCombinationsPattern.java
+++ ProjectoCandyDos/src/Logic/OnlyLinealCombinationsPattern.java
@@ -1,9 +1,12 @@
 package Logic;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 import Entities.Colour;
+import Entities.Entity;
 import Entities.PriorityEntity;
 import Entities.Stripped;
 
@@ -13,6 +16,39 @@ public class OnlyLinealCombinationsPattern extends Combination{
 		super(b);
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+	/**
+	 * basicamente, se redefine check full combinations para que asi, si se usa este tipo de combinaciones,
+	 * checkFullCombinations no devuelva las combianciones de cada bloque en particular
+	 * asi, no se puede romper algo que esta en forma de L o T, por ejemplo
+	 * lo que feu removido es lo que etsa comentado
+	 */
+	protected Entity checkFullCombination(Block block, Set<Block> combinationsOut) {
+        List<PriorityEntity> candys = new LinkedList<PriorityEntity>();
+        PriorityEntity candy = null;
+
+        Set<Block> combination = new HashSet<Block>();
+        candy = checkBlockCombination(block, combination);
+        if (candy != null) candys.add(candy);
+        combinationsOut.addAll(combination);
+       /* for (Block b : combination) {
+            Set<Block> currentCombinations = new HashSet<Block>();
+            candy = checkBlockCombination(b, currentCombinations);
+            if (candy != null)
+                candys.add(candy);
+            combinationsOut.addAll(currentCombinations);
+        }*/
+        // Get maximum priority.
+        candy = candys.size() > 0 ? candys.get(0) : null;
+        
+        for (PriorityEntity pe : candys) {
+            if (pe.getPriority() > candy.getPriority())
+                candy = pe;
+        }
+
+        return candy != null ? candy.getEntity() : null;
+    }
 	
 	protected PriorityEntity checkBlockCombination(Block block, Set<Block> combinationsOut) {
         Set<Block> combination = new HashSet<Block>();
