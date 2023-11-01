a5b24e6de78c02c0af4e907eee51f04ec7b8ce1e
Franco Piumatti <francopiumatti2004@gmail.com>
2023-11-01 09:38:34 -0300

    small change

--- ProjectoCandyDos/src/Logic/OnlyLinealCombinationsPattern.java
+++ ProjectoCandyDos/src/Logic/OnlyLinealCombinationsPattern.java
@@ -2,7 +2,10 @@ package Logic;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import Entities.Colour;
 import Entities.PriorityEntity;
+import Entities.Stripped;
 
 public class OnlyLinealCombinationsPattern extends Combination{
 
@@ -15,14 +18,30 @@ public class OnlyLinealCombinationsPattern extends Combination{
         Set<Block> combination = new HashSet<Block>();
         Set<Block> consecutiveH = new HashSet<Block>();
         Set<Block> consecutiveV = new HashSet<Block>();
+        Colour color = block.getEntity().getColour();
         consecutiveH = consecutiveH(block);
         consecutiveV = consecutiveV(block);
         combination.add(block);
-        
-        if(consecutiveV.size()>=2) combination.addAll(consecutiveV);
-        else if(consecutiveH.size()>=2) combination.addAll(consecutiveH);
-        
+        int hSize = consecutiveH.size();
+        int vSize = consecutiveV.size();
+        int row = block.getRow();
+        int column = block.getColumn();
         PriorityEntity entity = null;
+        
+        if (hSize == 3) {
+            entity = new PriorityEntity(new Stripped(row, column, color, true), 1);
+            combination.addAll(consecutiveH);
+        }
+        else if (vSize == 3) {
+            entity = new PriorityEntity(new Stripped(row, column, color, false), 1);
+            combination.addAll(consecutiveV);
+        }
+        else if(vSize>=2) 
+        	combination.addAll(consecutiveV);
+        else if(hSize>=2) 
+        	combination.addAll(consecutiveH);
+        
+        
         if (combination.size() >= 3)
             combinationsOut.addAll(combination);
         return entity;
