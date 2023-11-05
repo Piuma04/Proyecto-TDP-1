3b91d7c0abf682c92bb1a752ab1cb69f7061c764
Franco Piumatti <francopiumatti2004@gmail.com>
2023-11-05 17:16:22 -0300

    added template to make the remaining combinations

---
+++ ProjectoCandyDos/src/Combinations/OnlyLsCombinations.java
@@ -0,0 +1,22 @@
+package Combinations;
+
+import java.util.List;
+import java.util.Set;
+
+import Entities.Entity;
+import Logic.Block;
+import Logic.Board;
+
+public class OnlyLsCombinations extends BaseCombination{
+
+	public OnlyLsCombinations(Board b) {super(b);}
+
+	//TODO Implement the class correctly
+	
+	@Override
+	public Set<Block> checkCombinations(Set<Block> blocks, List<Entity> candysOut) {
+		
+		return null;
+	}
+
+}
---
+++ ProjectoCandyDos/src/Combinations/OnlyTsCombinations.java
@@ -0,0 +1,23 @@
+package Combinations;
+
+import java.util.List;
+import java.util.Set;
+
+import Entities.Entity;
+import Logic.Block;
+import Logic.Board;
+
+public class OnlyTsCombinations extends BaseCombination {
+
+	public OnlyTsCombinations(Board b) {super(b); }
+
+	
+	//TODO Implement the class correctly
+	
+	@Override
+	public Set<Block> checkCombinations(Set<Block> blocks, List<Entity> candysOut) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
