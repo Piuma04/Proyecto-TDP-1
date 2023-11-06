973ad0ecb239ccb5864aa161a183231043999273
Franco Piumatti <francopiumatti2004@gmail.com>
2023-11-05 22:43:56 -0300

    Now the bombs are fully functional NOTE: time is random, and the position of th timer can be adjusted

--- ProjectoCandyDos/src/Entities/Bomb.java
+++ ProjectoCandyDos/src/Entities/Bomb.java
@@ -3,19 +3,50 @@ package Entities;
 import java.util.LinkedList;
 import java.util.List;
 
-import Interfaces.Equivalent;
+import javax.swing.SwingUtilities;
 
+import Interfaces.Equivalent;
+import Interfaces.GameOverOnly;
 import Enums.Colour;
 
 import Logic.Block;
 import Logic.Board;
 
 public class Bomb extends Entity {
-    // private Timer timer;
-    public Bomb(int rowPosition, int columnPosition) {
+    
+	protected GameOverOnly finalized;
+	protected Runnable myTask;
+	protected Thread myThread;
+	protected int seconds;
+	
+    public Bomb(int rowPosition, int columnPosition, GameOverOnly game) {
         super(rowPosition, columnPosition, Colour.BOMB);
-        // timer = new Timer();
-        // timer.start();
+        finalized = game;
+        seconds = (int)Math.floor(Math.random() *(50 - 25 + 1) + 25) ;
+        
+        myTask = () -> {
+            while (seconds > 0 ) {
+            	
+            	if(gEntity!=null) gEntity.setText(String.valueOf(seconds));
+                try { Thread.sleep(1000); } catch (InterruptedException e) { System.out.println(e.getMessage()); }
+                seconds--;
+                if(gEntity!=null) gEntity.setText(String.valueOf(seconds));
+            }
+            finalized.finalLost();
+        };
+
+        myThread = new Thread(myTask);
+      
+        myThread.start();
+    }
+    
+    public void destroy() {
+    	
+       gEntity.setText("");
+        myThread.stop();
+        playGif(explosionGif);
+        setImage(null); 
+        
     }
 
     @Override public boolean isEquivalent(Equivalent e) { return e.isEquivalent(this); }
@@ -25,13 +56,12 @@ public class Bomb extends Entity {
     @Override public int getScore() { return 150; }
     
     public String toString() { return super.setStringColor("Q");}
+    
 
 	@Override
 	public List<Block> getDestroyables(Board b) {
 		List<Block> toReturn = new LinkedList<Block>();
 		toReturn.add(b.getBlock(row, column));
-		//TODO donde estaria para notificar que el juego termino
-		//timer.stop();
 		return toReturn;
 	}	
 }
